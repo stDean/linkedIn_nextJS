f26ba73b2b00549c1647e14686583dc2d372bbe4
Toba-dean <tobaogundimu@gmail.com>
2023-05-15 15:38:24 +0100

    added the header for the index page

---
+++ src/components/HeaderLink.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
+  return (
+    <div
+      className={`cursor-pointer flex flex-col justify-center items-center ${feed
+        ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1" : "text-gray-500 hover:text-gray-700"} ${active && "!text-black dark:!text-white"} ${hidden && "hidden md:inline-flex"
+        }`}
+    >
+      {
+        avatar ? (
+          <Icon className="!h-7 !w-7 lg:!-mb-1" />
+        ) : (
+          <Icon />
+        )
+      }
+      <h4 className='text-sm'>{text}</h4>
+    </div>
+  )
+}
+
+export default HeaderLink
