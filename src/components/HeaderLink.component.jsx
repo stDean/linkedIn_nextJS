b64c847436be66a345a0fa0a8876d621bcf0bd2e
Toba-dean <tobaogundimu@gmail.com>
2023-05-19 12:42:08 +0100

    added the sign out

--- src/components/HeaderLink.component.jsx
+++ src/components/HeaderLink.component.jsx
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
 
@@ -9,6 +9,7 @@ const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
       className={
         `cursor-pointer flex flex-col justify-center items-center ${feed ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1" : "text-gray-500 hover:text-gray-700"} ${active && "!text-black dark:!text-white"} ${hidden && "hidden md:inline-flex"}`
       }
+      onClick={() => avatar && signOut()}
     >
       {
         avatar ? (
